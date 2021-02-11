eaceb5ad5a8b32fca41feb5cacd43fc0f3cba319
yash0400 <yash0400@users.noreply.github.com>
2021-02-11 01:31:13 +0000

    Added basic html template

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/home')
 def home():
-    return("This is the Home Page")
+    return render_template("index.html")
 
 @app.route('/login')
 def login():

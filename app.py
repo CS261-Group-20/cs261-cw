f613ed82ec577b7e82fee164d999cb1f369acb11
yash0400 <yash0400@users.noreply.github.com>
2021-02-12 17:45:26 +0000

    added redirect '/' to '/home'

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return redirect('/home')
 
 @app.route('/home')
 def home():
